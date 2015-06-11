a8d7e15ab6c84f8829b5661efd61e19f84247a4e
hongye <185399@qq.com>
2015-06-11 17:34:25 +0800

    20150611-2

---
+++ src/main/java/com/fung/enumtest/Test1.java
@@ -0,0 +1,21 @@
+package com.fung.enumtest;
+
+public class Test1 {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Signal color = Signal.RED;
+		
+		 System.out.println(color);
+	}
+	
+	enum Signal {
+        GREEN, YELLOW, RED
+    }
+	
+	
+
+}
